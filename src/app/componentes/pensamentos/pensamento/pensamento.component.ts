afe37971eeb57e605aa51303bc8558f964859677
RenanCruz <renancruz199@gmail.com>
2023-10-03 18:42:56 -0300

    ngFor ngIf ngClass

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent {
+
+  /**
+   Diretiva input para dizer que esse objeto recebera os dados de um outro componente 
+   com os seguintes atributos
+   */
+  @Input() pensamento = {
+    conteudo: "",
+    autoria: "",
+    modelo: ''
+  }
+
+  larguraPensamento(): string{
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+}
