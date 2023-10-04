310f2c0efd45e888e6a7985e435c9c8c6b988c60
RenanCruz <renancruz199@gmail.com>
2023-10-03 22:36:16 -0300

    Edição de Pensamentos e Conclusão do Curso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   //Rota padrão da Pagina
@@ -24,6 +25,10 @@ const routes: Routes = [
   {
   path: 'pensamento/excluirPensamento/:id',
   component: ExcluirPensamentoComponent
+  },
+  {
+  path: 'pensamento/editarPensamento/:id',
+  component: EditarPensamentoComponent
   }
 ];
 
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -12,12 +12,12 @@ export class PensamentoService {
   private readonly API = "http://localhost:3000/pensamentos"
   constructor(private http: HttpClient) { }
 
-  listar(): Observable<Pensamento[]>{
+  listar(): Observable<Pensamento[]> {
     return this.http.get<Pensamento[]>(this.API)
   }
 
-  cadastrar(pensamento: Pensamento): Observable<Pensamento>{
-    return this.http.post<Pensamento>(this.API,pensamento)
+  cadastrar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, pensamento)
   }
 
   excluir(id: number): Observable<Pensamento> {
@@ -25,8 +25,13 @@ export class PensamentoService {
     return this.http.delete<Pensamento>(url)
   }
 
-  buscarPorId(id: number): Observable<Pensamento>{
+  buscarPorId(id: number): Observable<Pensamento> {
     const url = `${this.API}/${id}`
     return this.http.get<Pensamento>(url)
+  }
+
+  editar(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API}/${pensamento.id}`
+    return this.http.put<Pensamento>(url, pensamento)
   }
 }
