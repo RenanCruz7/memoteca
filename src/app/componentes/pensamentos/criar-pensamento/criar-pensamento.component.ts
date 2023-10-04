05fae41472f9ab144aac35975e53c87f15a606e7
RenanCruz <renancruz199@gmail.com>
2023-10-04 18:51:11 -0300

    formBuilder

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,19 +11,25 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent {
 
-  pensamento: Pensamento ={
-    conteudo: '',
-    autoria:'',
-    modelo:'modelo1'
+  formulario!: FormGroup
+
+  ngOnInit(): void{
+    this.formulario = this.formBuilder.group({
+      conteudo: ['aaaaaaaaaaaaaaaaaaaaa'],
+      autoria: ['Messi'],
+      modelo: ['modelo1']
+    })
   }
+
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
     ){}
 
   criarPensamento(){
-    this.service.cadastrar(this.pensamento).subscribe(()=>{
+    this.service.cadastrar(this.formulario.value).subscribe(()=>{
       this.router.navigate(['/listarPensamento'])
     })
   }
